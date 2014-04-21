4c91fd3f4beaddea03dba2a8c6aab430c2708ad0
James Johnson <johnsojr@gmail.com>
2014-04-20 22:27:56 -0500

    Apache config info added

--- local-script.sh
+++ local-script.sh
@@ -1,4 +1,16 @@
 #sudo npm -g install gulp
 
+#Setup Apache
+sudo curl -L -o '/etc/apache2/sites-available/hello-laravel.conf' https://gist.githubusercontent.com/johnsojr/60ebc48fd87ebebedec2/raw/5d60e7e230c4a6d8986f04cfb9313820e158948f/gistfile1.apacheconf
+
+# Turn off any default sites
+sudo a2dissite 000-default
+sudo a2dissite 192.168.22.10.zip.io
+sudo a2disssite default-ssl
+
+# Turn on new hello-laravel site
+sudo a2ensite hello-laravel
+sudo service apache2 reload
+
 # Support for ruby-sass
 sudo gem install sass
